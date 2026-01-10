c0d4cc7acd5ff12e0a703322ff9da0b2fb63649b
teotexe <teotechplus@gmail.com>
2026-01-10 13:25:53 +0100

    Added example program

---
+++ example-program.asm
@@ -0,0 +1,8 @@
+LDA 5    # Comment 1
+ADD 6    // Comment 2
+OUT    
+HLT    
+DAT 0
+DAT 10
+DAT 4
+
--- program.asm
+++
@@ -1,8 +0,0 @@
-LDA 5    # Ok fine
-ADD 6    // COmment
-OUT    
-HLT    
-DAT 0
-DAT 10
-DAT 4
-
